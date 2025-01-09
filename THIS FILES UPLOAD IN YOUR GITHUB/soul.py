228620db6bd1aa46b873ed82721e7761bb6a42f2
Vikramhudjs <vikramxpatelyt24@gmail.com>
2025-01-09 18:02:16 +0530

    Update soul.py

--- THIS FILES UPLOAD IN YOUR GITHUB/soul.py
+++ THIS FILES UPLOAD IN YOUR GITHUB/soul.py
@@ -15,10 +15,10 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 loop = asyncio.get_event_loop()
 
 # Bot Configuration: Set with Authority
-TOKEN = 'YOUR TOKEN'
-ADMIN_USER_ID = USER ID YOUR
+TOKEN = '7893542521:AAGtLqMRt9F92kPr6kPlSLSjKdLR9elPTZc'
+ADMIN_USER_ID = 7516328519
 MONGO_URI = 'mongodb+srv://sharp:sharp@sharpx.x82gx.mongodb.net/?retryWrites=true&w=majority&appName=SharpX'
-USERNAME = "@VIPMODSXADMIN"  # Immutable username for maximum security
+USERNAME = "@vikram6061"  # Immutable username for maximum security
 
 # Attack Status Variable to Control Single Execution
 attack_in_progress = False
